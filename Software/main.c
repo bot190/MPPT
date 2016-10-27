8ea43870e962961971c976738d2865bea595340a
Johnathan <jwaohnathan@live.com>
2016-10-27 14:38:27 -0400

    Clock Configuration Get
    Framework further expanded, started config code by adding clock
    configuration.

--- Software/main.c
+++ Software/main.c
@@ -21,6 +21,20 @@ int main(void) {
 	//	Configuration & Initialization
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 
+    //Set Clock/Timer
+    DCOCTL = 0b11100000;	//DCOx = 7, MODx = 0
+    BCSCTL1 |= 0b1111;		//RSELx = 15
+    BCSCTL1 &= 0b001111;	//DIVx = 0, clock divider = 1 (default)
+    BCSCTL2 = 0b00000000;	// MCLK & SMCLK from DCO, both divided by 1, DCO resistor internal
+
+    //Set ADC
+    //Set GPIO
+
+    //Start Timers
+    //Enable ADC's
+
+    //Enable Individual Interrupts
+
     BIS_SR(GIE);	//Global Interrupt Enable... I think.  Not tested.  Do all config before this line.
 
     //	Code Body
--- Software/sweep.c
+++ Software/sweep.c
@@ -15,5 +15,6 @@
 
 int sweep(void) {
 
+
 	return 0;
 }
