8f7c6570b7fa281fcd3778f26ee2c93074edfc00
Ben Beauregard <bot190@gmail.com>
2016-11-01 14:45:46 -0400

    Updated Project to use MSP430G2553, including GPIO.

--- Software/main.c
+++ Software/main.c
@@ -27,26 +27,35 @@ int main(void) {
     BCSCTL2 = 0b00000000;	// MCLK & SMCLK from DCO, both divided by 1, DCO resistor internal
 
     // PIN MAP
-    // P1.0 - I - Current Sense
-    // P1.1 - O - Gate Driver MPPT MOSFET
-    // P1.2 - O - Gate Drive 2nd MOSFET
-    // P1.3 - I - MPPT Voltage
-    // P1.4 - I - Output Voltage
+    // P1.0 - I - Current Sense - A0
+    // P1.1 - I - MPPT Voltage - A1
+    // P1.2 - I - Output Voltage - A2
+    // P1.3 - O - UNUSED
+    // P1.4 - O - UNUSED
     // P1.5 - O - UNUSED
     // P1.6 - O - UNUSED
     // P1.7 - O - UNUSED
+    // P2.0 - O - UNUSED
+    // P2.1 - O - Gate Driver MPPT MOSFET - TA1.1
+    // P2.2 - O - UNUSED
+    // P2.3 - O - UNUSED
+    // P2.4 - O - Gate Drive 2nd MOSFET - TA1.2
+    // P2.5 - O - UNUSED
     // P2.6 - O - UNUSED
     // P2.7 - O - UNUSED
 
     //Configure GPIO
-    P1DIR = (BIT1 | BIT2);					// Set P1.1 and P1.2 to output direction
-    P1SEL = (BIT1 | BIT2);					// Set P1.1 and P1.2 to primary function
+    P2DIR = (BIT1 | BIT4);		// Set P2.1 and P2.4 to output direction
+    P2SEL = (BIT1 | BIT4);		// Set P2.1 and P2.4 to primary function
 
     // Configure unused pins
-    P1DIR |= (BIT5 | BIT6 | BIT7);
-    P1OUT |= (BIT5 | BIT6 | BIT7);
-    P2DIR = (BIT6 | BIT7);
-    P2OUT = 0x00;
+    P1DIR = (BIT3 | BIT4 | BIT5 | BIT6 | BIT7);
+    P1OUT = (BIT3 | BIT4 | BIT5 | BIT6 | BIT7);
+    P2DIR |= (BIT0 | BIT2 | BIT3 | BIT5 | BIT6 | BIT7);
+    P2OUT |= (BIT0 | BIT2 | BIT3 | BIT5 | BIT6 | BIT7);
+    P3DIR = 0xF;
+    P3OUT = 0xF;
+    P3REN = 0xF;
 
     //Configure ADC - Run on MCLK - 16MHz/4 = 4MHzx64 = 16uS
     ADC10CTL0 = (SREF_0 | ADC10SHT_3 | ADC10ON | ADC10IE);		// Use Vcc and Vss, S&H Time: 64*ADC10CLKs, turn on ADC, enable interrupt
@@ -54,6 +63,9 @@ int main(void) {
     // Not using the DTC
 
     //Configure Timers - SMCLK
+    // PWM at 50KHz with 16MHz Clock
+    // Count to 320-1
+
 
     //Enable ADC's
 
