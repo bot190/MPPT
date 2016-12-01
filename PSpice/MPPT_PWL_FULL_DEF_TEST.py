ef4080e666586f6a859a3fa6f45657c38e2bd78c
aflynn73 <aflynn73@gmail.com>
2016-12-01 15:31:47 -0500

    Starting to make adjustments for 100/320 duty cycle step
    changing python script to write library file instead of copy and past
    method. Simulated and only worked up to just under 500ms instead of the
    1580ms or so that the code defines. Need to check python script for what
    makes it stop at 500ms

--- PSpice/MPPT_PWL_FULL_DEF_TEST.py
+++ PSpice/MPPT_PWL_FULL_DEF_TEST.py
@@ -10,9 +10,9 @@
 frequency = 50000       #freqency (Hertz)
 period = 1000/frequency    #length of period (m)
 pstart = 1              #starting duty cycle percentage
-pend = 99             #ending duty cycle percentage
+pend = 99               #ending duty cycle percentage
 pcurrent = 1            #place holder for current duty cycle value
-pstep = 1               #percentage to go up for each tstep 
+pstep = 0.3125          #percentage to go up for each tstep 
 tstart = 0              #start time (ms)
 tend = 0                #end time (ms) just to inform user
 tstep = 5               #time in ms between voltage changes
@@ -25,10 +25,16 @@ DT = (pcurrent/100)*period  #pulse width
 
 string = "+ {0:.4f}ms,{1:.1f}V {2:.4f}ms,{3:.1f}"
 
+f=open('\\\\storage.wpi.edu\\home\\My_Documents\\MPPT_GIT\\MPPT\\PSpice\\BRUTE_FORCE_VOLTAGE_SOURCE_320.LIB', 'w')
+
+print("*BRUTE_FORCE_VOLTAGE_SOURCE_320", file=f)
+print(".SUBCKT PWM VOUT GND", file=f)
+print("VTHR VOUT GND PWL(", file=f)
+
 while (pcurrent < pend):
     while (telap2-telap1 < tstep):
-        print(string.format(telap2,vhigh,telap2+DT-ttrans,vhigh))
-        print(string.format(telap2+DT,vlow,telap2+period-ttrans,vlow))
+        print(string.format(telap2,vhigh,telap2+DT-ttrans,vhigh), file=f)
+        print(string.format(telap2+DT,vlow,telap2+period-ttrans,vlow), file=f)
         telap2 += period
     pcurrent += pstep
     DT = (pcurrent/100)*period
@@ -38,3 +44,8 @@ while (pcurrent < pend):
 ##    print(string.format(time,voltA,time+tstep-ttrans,voltA))
 ##    voltA-=0.1
 ##    time+=tstep
+
+print(")", file=f)
+print(".ENDS", file=f)
+print("*END PWM", file=f)
+f.close()
