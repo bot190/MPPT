0cea1e7ca22587c113e6fdb77a289d86fa42b8ba
aflynn73 <aflynn73@gmail.com>
2016-12-05 15:27:01 -0500

    Changed python script
    script now outputs a second file which representes the duty cycle
    percentage as a voltage between 0 and 100.

--- PSpice/MPPT_PWL_FULL_DEF_TEST.py
+++ PSpice/MPPT_PWL_FULL_DEF_TEST.py
@@ -8,13 +8,12 @@
 # def vars
 # note that time is in ms
 frequency = 50000       #freqency (Hertz)
-period = 1000/frequency    #length of period (m)
+period = 1000/frequency    #length of period (ms)
 pstart = 1              #starting duty cycle percentage
 pend = 99               #ending duty cycle percentage
 pcurrent = 1            #place holder for current duty cycle value
 pstep = 0.3125          #percentage to go up for each tstep 
 tstart = 0              #start time (ms)
-tend = 0                #end time (ms) just to inform user
 tstep = 5               #time in ms between voltage changes
 telap1 = 0              #elapsed time in ms
 telap2 = 0              #elapsed time in ms
@@ -24,14 +23,21 @@ vlow = 0                #lower voltage (output low)
 DT = (pcurrent/100)*period  #pulse width
 
 string = "+ {0:.4f}ms,{1:.1f}V {2:.4f}ms,{3:.1f}"
+string2 = "+ {0:.4f}ms, {1:.4f}V"
 
 f=open('\\\\storage.wpi.edu\\home\\My_Documents\\MPPT_GIT\\MPPT\\PSpice\\BRUTE_FORCE_VOLTAGE_SOURCE_320.LIB', 'w')
+f2=open('\\\\storage.wpi.edu\\home\\My_Documents\\MPPT_GIT\\MPPT\\PSpice\\BRUTE_FORCE_VOLTAGE_SOURCE_320_PERCENT.LIB', 'w')
 
 print("*BRUTE_FORCE_VOLTAGE_SOURCE_320", file=f)
 print(".SUBCKT PWM VOUT GND", file=f)
 print("VTHR VOUT GND PWL(", file=f)
 
+print("*BRUTE_FORCE_VOLTAGE_SOURCE_320_PERCENT", file=f2)
+print(".SUBCKT PERCENT VOUT GND", file=f2)
+print("VTHR VOUT GND PWL(", file=f2)
+
 while (pcurrent < pend):
+    print(string2.format(telap2,pcurrent),file=f2)
     while (telap2-telap1 < tstep):
         print(string.format(telap2,vhigh,telap2+DT-ttrans,vhigh), file=f)
         print(string.format(telap2+DT,vlow,telap2+period-ttrans,vlow), file=f)
@@ -49,3 +55,8 @@ print(")", file=f)
 print(".ENDS", file=f)
 print("*END PWM", file=f)
 f.close()
+
+print(")", file=f2)
+print(".ENDS", file=f2)
+print("*END PERCENT", file=f2)
+f2.close()
