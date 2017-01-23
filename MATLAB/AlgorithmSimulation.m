66d34c996ecedb7d36a02f624b336711e40b168e
Johnathan <jwaohnathan@live.com>
2017-01-23 11:50:41 -0500

    MATLAB Algorithm Simulation Added
    Preliminary work, not functional yet

---
+++ MATLAB/AlgorithmSimulation.m
@@ -0,0 +1,8 @@
+%   MATLAB script to prove functionality of MPPT algorithm
+%   Authors: Andrew Flynn, Ben Beauregard, Johnathan Adams
+
+%   Flynn's Code to load Experimental Data
+
+%   Ben's MPPT Algorithm
+
+%   Graph dat
