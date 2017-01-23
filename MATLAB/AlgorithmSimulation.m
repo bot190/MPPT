96d13f951890c6541c3d16ccf528cb46f863e054
Johnathan <jwaohnathan@live.com>
2017-01-23 12:36:55 -0500

    Data Reading
    implemented data reading

--- MATLAB/AlgorithmSimulation.m
+++ MATLAB/AlgorithmSimulation.m
@@ -2,7 +2,9 @@
 %   Authors: Andrew Flynn, Ben Beauregard, Johnathan Adams
 
 %   Flynn's Code to load Experimental Data
+ivcurve
 
 %   Ben's MPPT Algorithm
 
-%   Graph dat
+
+%   Graph data
--- MATLAB/ivcurve.m
+++ MATLAB/ivcurve.m
@@ -1,14 +1,20 @@
 clear; 
-fid = fopen('R:/solarcell7.txt', 'r') ; % Open source file.
-fgetl(fid) ;                                  % Read/discard line.
-fgetl(fid) ;                                  % Read/discard line.
-buffer = fread(fid, Inf) ;                    % Read rest of the file.
-fclose(fid);
-fid = fopen('R:/no_header.txt', 'w')  ;   % Open destination file.
-fwrite(fid, buffer) ;                         % Save to file.
-fclose(fid) ;
+fid = fopen('SolarSimulator_30V_CurrentRamp.txt', 'r') ; % Open source file.
+if fid == -1
+    disp('ERROR 404: File Not Found') ;
+else
+    fgetl(fid) ;                                  % Read/discard line.
+    fgetl(fid) ;                                  % Read/discard line.
+    buffer = fread(fid, Inf) ;                    % Read rest of the file.
+    fclose(fid);
+    fid = fopen('_temp.txt', 'w')  ;   % Open destination file.
+    fwrite(fid, buffer) ;                         % Save to file.
+    fclose(fid) ;
+    A = tdfread('_temp.txt') ;
+    delete('_temp.txt') ;
+end
 
-A = tdfread('R:/no_header.txt');
-plot(A.Voltage_0x5BV0x5D, A.x0x23_Current_0x5BA0x5D,'-o')
-figure
-plot(A.Voltage_0x5BV0x5D, A.Voltage_0x5BV0x5D.*A.x0x23_Current_0x5BA0x5D,'-o')
+%Plotting part that we don't need but is useful as reference
+%plot(A.Voltage_0x5BV0x5D, A.x0x23_Current_0x5BA0x5D,'-o')
+%figure
+%plot(A.Voltage_0x5BV0x5D, A.Voltage_0x5BV0x5D.*A.x0x23_Current_0x5BA0x5D,'-o')
