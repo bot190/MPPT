6b4b7dab9b259613d48151caffe48012acb4feac
Ben Beauregard <bot190@gmail.com>
2017-01-26 14:18:14 -0500

    Add functionality for resetting sweep, LED indicates sweep in progress

--- Software/sweep.c
+++ Software/sweep.c
@@ -19,15 +19,23 @@
 #include "msp430.h"
 
 int sweep(volatile char *DCTL) {
-    // If we just hit 100%, mark sweep complete
-    // else have we done the entire sweep yet?
+
+    // If button has been pressed, lets reset the Sweep control register
+    if (P1IN & BIT1) {
+        *DCTL &= ~SWEEP_COMPLETE;
+    }
+    // Only need to do something if sweep is not complete
     if ((*DCTL & SWEEP_COMPLETE) == 0) {
+        // Turn on LED at P1.6 when sweeping
+        P1OUT |= (BIT6);
         power = i_mppt * v_mppt;
         // New power > old power, save duty cycle
         if (power > max_power) {
             max_power_duty_cycle = TA1CCR1;
             max_power = power;
         }
+        // We've reached 100% duty cycle, mark as complete,
+        // Set duty cycle to maximum power point
         if (mppt_duty_cycle == 320) {
             *DCTL |= SWEEP_COMPLETE;
             mppt_duty_cycle = max_power_duty_cycle;
@@ -35,6 +43,9 @@ int sweep(volatile char *DCTL) {
             // Haven't completed sweep yet, increment duty cycle
             mppt_duty_cycle += SWEEPINC;
         }
+    } else {
+        // Turn off LED at P1.6 when sweeping
+        P1OUT &= (~BIT6);
     }
 
     return mppt_duty_cycle;
